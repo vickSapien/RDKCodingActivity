ecc1f6fe9d2b8c71460ce63fa4bde0984e34b171
Victor <vpineda@ycp.edu>
2025-11-12 14:46:05 -0500

    initial project creation

---
+++ Coding Activity 2 - Pseudocode to code.py
@@ -0,0 +1,20 @@
+'''
+FUNCTION sortAndFindMedian(numbers)
+CALL sort(numbers)
+DEFINE n AS length of numbers
+IF n MOD 2 = 0
+RETURN (numbers[n/2 - 1] + numbers[n/2 ) / 2
+ELSE
+RETURN numbers[n/2]
+ENDIF
+ENDFUNCTION
+FUNCTION sort(numbers)
+// Implement a sorting algorithm (e.g., bubble sort, selection sort, etc.)
+// Sort the 'numbers' array in ascending order
+// Pseudocode for the sorting algorithm is not provided; implement as per your
+understanding
+ENDFUNCTION
+'''
+
+
+
